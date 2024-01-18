bf3f891120d4025f9a3b9cd1b95c918a29d158fa
“Firstname <fearghuskennedy@fearghuss-MacBook-Air.local>
2024-01-18 19:12:25 +0000

    hello

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
-    about_page_link = '<a href="/rango/about/">About Page</a>'
-    response_content = f"Rango says hey there partner! Check out the {about_page_link}."
-    return HttpResponse(response_content)
+
 def about(request):
     index_page_link = '<a href = "/rango/">Index page</a>'
     response_content  = f"Rango says here is the about page. Return to Index page {index_page_link}."
     return HttpResponse(response_content)
 
+def index(request):
+# Construct a dictionary to pass to the template engine as its context.
+# Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier. # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
