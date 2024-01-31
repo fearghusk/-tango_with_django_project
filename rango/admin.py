44d37d2324258d9a7a128a1192c3ca5bab2c5d6c
“Firstname <fearghuskennedy@fearghuss-MacBook-Air.local>
2024-01-31 16:06:37 +0000

    chapterSix

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Register the models with their respective admins
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
